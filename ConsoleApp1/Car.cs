748688b6f12cda0cffec10403436f69f7916674b
bhaumikpatel851 <134671922+bhaumikpatel851@users.noreply.github.com>
2024-07-18 15:50:06 -0400

    Car
    In it has information about the car.

---
+++ ConsoleApp1/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Car : Vehicle
+{
+    public int Seats { get; set; }
+    public string EngineType { get; set; }
+    public string Transmission { get; set; }
+    public bool Convertible { get; set; }
+
+    public Car(string model, string manufacturer, int year, double rentalPrice, int seats, string engineType, string transmission, bool convertible)
+        : base(model, manufacturer, year, rentalPrice)
+    {
+        Seats = seats;
+        EngineType = engineType;
+        Transmission = transmission;
+        Convertible = convertible;
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Seats: {Seats}");
+        Console.WriteLine($"Engine Type: {EngineType}");
+        Console.WriteLine($"Transmission: {Transmission}");
+        Console.WriteLine($"Convertible: {Convertible}");
+    }
+}
