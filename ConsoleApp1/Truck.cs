08c71632e25c4080627c10b1e67e9c57a8176a19
bhaumikpatel851 <134671922+bhaumikpatel851@users.noreply.github.com>
2024-07-18 18:20:03 -0400

    Truck
    In this it is about truck information

---
+++ ConsoleApp1/Truck.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+public class Truck : Vehicle
+{
+    public double Capacity { get; set; }
+    public string TruckType { get; set; }
+    public bool FourWheelDrive { get; set; }
+
+    public Truck(string model, string manufacturer, int year, double rentalPrice, double capacity, string truckType, bool fourWheelDrive)
+        : base(model, manufacturer, year, rentalPrice)
+    {
+        Capacity = capacity;
+        TruckType = truckType;
+        FourWheelDrive = fourWheelDrive;
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Capacity: {Capacity}");
+        Console.WriteLine($"Truck Type: {TruckType}");
+        Console.WriteLine($"Four Wheel Drive: {FourWheelDrive}");
+    }
+}
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+748688b6f12cda0cffec10403436f69f7916674b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e69030a375736551422885c531f7dbc8e42cdbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
