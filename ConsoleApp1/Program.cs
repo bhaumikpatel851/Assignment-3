a65bc88c2f4fdaca97f5c966fb4175cc5117c89f
bhaumikpatel851 <134671922+bhaumikpatel851@users.noreply.github.com>
2024-07-18 21:14:35 -0400

    Vehicle rental whole process
    In this I have inputed one car,truck,motorcycle so it runs shows how it working.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car1 = new Car("Brezza", "Maruti Suzuki", 2022, 70.0, 5, "Petrol", "Automatic", false);
+        Truck truck1 = new Truck("Ashok Leyland", "Ashok Leyland", 2021, 150.0, 3000, "Cargo", false);
+        Motorcycle motorcycle1 = new Motorcycle("Splender", "Hero", 2020, 50.0, 1000, "Petrol", false);
+
+        Console.WriteLine("Car Details:");
+        car1.DisplayDetails();
+        Console.WriteLine();
+
+        Console.WriteLine("Truck Details:");
+        truck1.DisplayDetails();
+        Console.WriteLine();
+
+        Console.WriteLine("Motorcycle Details:");
+        motorcycle1.DisplayDetails();
+        Console.WriteLine();
+
+        RentalAgency agency = new RentalAgency(15);
+
+        agency.AddVehicle(car1);
+        agency.AddVehicle(truck1);
+        agency.AddVehicle(motorcycle1);
+
+        Console.WriteLine("Renting out the motorcycle...");
+        agency.RentVehicle("Splender");
+
+        Console.WriteLine($"Total Revenue: ${agency.TotalRevenue}");
+    }
+}
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e69030a375736551422885c531f7dbc8e42cdbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95f6e3c1ecca34f641f3b0f430339ca42eff7fd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
