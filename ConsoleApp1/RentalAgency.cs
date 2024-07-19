95f6e3c1ecca34f641f3b0f430339ca42eff7fd1
bhaumikpatel851 <134671922+bhaumikpatel851@users.noreply.github.com>
2024-07-18 20:40:32 -0400

    RentalAgency
    In this there is information of rentalagency.

---
+++ ConsoleApp1/RentalAgency.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class RentalAgency
+{
+    private Vehicle[] Fleet { get; set; }
+    public double TotalRevenue { get; private set; }
+
+    public RentalAgency(int capacity)
+    {
+        Fleet = new Vehicle[capacity];
+        TotalRevenue = 0;
+    }
+
+    public void AddVehicle(Vehicle vehicle)
+    {
+        for (int i = 0; i < Fleet.Length; i++)
+        {
+            if (Fleet[i] == null)
+            {
+                Fleet[i] = vehicle;
+                break;
+            }
+        }
+    }
+
+    public void RemoveVehicle(string model)
+    {
+        for (int i = 0; i < Fleet.Length; i++)
+        {
+            if (Fleet[i] != null && Fleet[i].Model == model)
+            {
+                Fleet[i] = null;
+                break;
+            }
+        }
+    }
+
+    public void RentVehicle(string model)
+    {
+        for (int i = 0; i < Fleet.Length; i++)
+        {
+            if (Fleet[i] != null && Fleet[i].Model == model)
+            {
+                TotalRevenue += Fleet[i].RentalPrice;
+                Fleet[i] = null;
+                break;
+            }
+        }
+    }
+}
